2304473069a40ff40d4ebc2eb8503845fb8ba008
Andrew Fechner <arfechner@gmail.com>
2017-06-13 23:58:13 -0400

    Code cleanup and helper function refactor

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+// 1. This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+let firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
+// 2. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
+let player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-    height: '390',
-    width: '640',
     videoId: 'QrGrOK8oZG8'
-    // events: {
-    //   'onReady': onPlayerReady
-      // 'onStateChange': onPlayerStateChange
-    // }
   });
 }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// // 5. The API calls this function when the player's state changes.
-// //    The function indicates that when playing a video (state=1),
-// //    the player should play for six seconds and then stop.
-// var done = false;
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
-// function stopVideo() {
-//   player.stopVideo();
-// }
-
+// 3. This is the function called when the button is clicked.
+//    It checks to see if the video is playing, and if it is,
+//    it grabs the current time of the video, cleans it up, and
+//    prepends the current time to the list of times below the video.
 const timeGrabber = () => {
   if (player.getPlayerState() === 1) {
     let timestamp = player.getCurrentTime();
@@ -48,13 +27,11 @@ const timeGrabber = () => {
   }
 }
 
+// 4. This helper function takes a number of seconds and converts
+//    it into a more readable mm:ss format
 const timeCleaner = (timestamp) => {
   let minutes = ~~(timestamp/60);
-  let seconds = timestamp-(minutes*60);
-  seconds = seconds < 10 ? '0'+(''+seconds).slice(0,1) : (''+seconds).slice(0,2);
+  let seconds = Math.round(timestamp-(minutes*60));
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
   return `${minutes}:${seconds}`;
 }
-
-
-
-
